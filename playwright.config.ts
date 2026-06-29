b92457de5166ba08cb56b3dc31243f4041603952
Jose Gomez <gomez1993jose@gmail.com>
2026-06-29 20:43:39 +0800

    rename path

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ import path from 'path';
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
-const localFilePath = path.resolve(__dirname, '../PlaywrightNexus'); 
+const localFilePath = path.resolve(__dirname, '../'); 
 const isCI = !!process.env.CI;
 
 export default defineConfig({
